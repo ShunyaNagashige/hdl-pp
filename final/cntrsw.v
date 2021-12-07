e9fc2a7da7c46aad09ab4e6f469fde900ec709b5
ShunyaNagashige <nagashige190441091@gmail.com>
2021-12-07 11:11:58 +0900

    とりあえずこみっと

--- final/cntrsw.v
+++ final/cntrsw.v
@@ -1,20 +1,21 @@
 // Synchronous BCD counter with asynchronous reset
 module CNTRSW(CLOCK, RESET, EN, RSW, CNT);
 input CLOCK, RESET, EN;
+input [3:0] RSW;
 output [3:0] CNT;
-output BO;
+// output BO;
 
 reg [3:0] CNT;
 
 always @(posedge CLOCK or posedge RESET) begin
 // TODO:?
     if(RESET == 1'b1)
-        CNT <= 4'd0;
+        CNT <= RSW;
     else if(EN == 1'b1) begin
-        if(CNT == 4'd0)
-            CNT <= 4'd9;
-        else
-            CNT <= CNT - 4'd1; 
+        // if(CNT == 4'd0)
+        //     CNT <= RSW;
+        // else
+        CNT <= CNT - 4'd1; 
     end
 end
 
